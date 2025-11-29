f6e486073d431ce3f8e06f1e449b025b782f20c2
lynnwallenstein <lynnwallenstein@users.noreply.github.com>
2025-11-29 04:43:59 +0000

    Cleaning up the code base lynnwallenstein/lynnio-hugo@ac78eac6053314fee6087595d4cd4d49a3d2ed17

--- fonts/social-circle/ss-social.js
+++ fonts/social-circle/ss-social.js
@@ -15,113 +15,113 @@ if (
   !/(IEMobile)/.test(navigator.userAgent)
 ) {
   if (/Android 4\.[2-9].*Version\/4/.test(navigator.userAgent)) {
-    var ss_android = document.createElement('style');
+    var ss_android = document.createElement("style")
     ss_android.innerHTML =
-      '.ss-icon,[class^="ss-"],[class*=" ss-"],[class^="ss-"]:before,[class*=" ss-"]:before,[class^="ss-"].right:after[class*=" ss-"].right:after{text-rendering:auto!important}';
-    document.body.appendChild(ss_android);
+      '.ss-icon,[class^="ss-"],[class*=" ss-"],[class^="ss-"]:before,[class*=" ss-"]:before,[class^="ss-"].right:after[class*=" ss-"].right:after{text-rendering:auto!important}'
+    document.body.appendChild(ss_android)
   }
 
   var ss_set = {
-    'five hundred pixels': '\uF642',
-    fivehundredpixels: '\uF642',
-    'five hundred px': '\uF642',
-    'github octocat': '\uF671',
-    'stack overflow': '\uF672',
-    stackoverflow: '\uF672',
-    fivehundredpx: '\uF642',
-    githuboctocat: '\uF671',
-    kickstarter: '\uF681',
-    'app dot net': '\uF614',
-    'google plus': '\uF613',
-    googleplus: '\uF613',
-    foursquare: '\uF690',
-    soundcloud: '\uF6B3',
-    letterboxd: '\uF632',
-    blackberry: '\uF6F4',
-    delicious: '\uF655',
-    posterous: '\uF623',
-    pinterest: '\uF650',
-    microsoft: '\uF6F1',
-    'thumbs up': '\uD83D\uDC4D',
-    telephone: '\uD83D\uDCDE',
-    appdotnet: '\uF614',
-    wordpress: '\uF621',
-    instagram: '\uF641',
-    facebook: '\uF610',
-    thumbsup: '\uD83D\uDC4D',
-    readmill: '\uF652',
-    pinboard: '\uF654',
-    dribbble: '\uF660',
-    envelope: '\u2709',
-    'google +': '\uF613',
-    linkedin: '\uF612',
-    twitter: '\uF611',
-    approve: '\uD83D\uDC4D',
-    behance: '\uF661',
-    youtube: '\uF630',
-    blogger: '\uF622',
-    dropbox: '\uF653',
-    octocat: '\uF671',
-    android: '\uF6F3',
-    'google+': '\uF613',
-    'last fm': '\uF6B2',
-    'app net': '\uF614',
-    windows: '\uF6F2',
-    spotify: '\uF6B1',
-    flickr: '\uF640',
-    lastfm: '\uF6B2',
-    zerply: '\uF615',
-    appnet: '\uF614',
-    paypal: '\uF680',
-    tumblr: '\uF620',
-    github: '\uF670',
-    svpply: '\uF651',
-    reddit: '\uF616',
-    share: '\uF601',
-    phone: '\uD83D\uDCDE',
-    apple: '\uF8FF',
-    vimeo: '\uF631',
-    email: '\u2709',
-    steam: '\uF617',
-    quora: '\uF624',
-    '500px': '\uF642',
-    skype: '\uF6A0',
-    like: '\uD83D\uDC4D',
-    mail: '\u2709',
-    call: '\uD83D\uDCDE',
-    link: '\uD83D\uDD17',
-    rdio: '\uF6B0',
-    yelp: '\uF691',
-    etsy: '\uF682',
-    vine: '\uF633',
-    rss: '\uE310',
-  };
+    "five hundred pixels": "\uF642",
+    fivehundredpixels: "\uF642",
+    "five hundred px": "\uF642",
+    "github octocat": "\uF671",
+    "stack overflow": "\uF672",
+    stackoverflow: "\uF672",
+    fivehundredpx: "\uF642",
+    githuboctocat: "\uF671",
+    kickstarter: "\uF681",
+    "app dot net": "\uF614",
+    "google plus": "\uF613",
+    googleplus: "\uF613",
+    foursquare: "\uF690",
+    soundcloud: "\uF6B3",
+    letterboxd: "\uF632",
+    blackberry: "\uF6F4",
+    delicious: "\uF655",
+    posterous: "\uF623",
+    pinterest: "\uF650",
+    microsoft: "\uF6F1",
+    "thumbs up": "\uD83D\uDC4D",
+    telephone: "\uD83D\uDCDE",
+    appdotnet: "\uF614",
+    wordpress: "\uF621",
+    instagram: "\uF641",
+    facebook: "\uF610",
+    thumbsup: "\uD83D\uDC4D",
+    readmill: "\uF652",
+    pinboard: "\uF654",
+    dribbble: "\uF660",
+    envelope: "\u2709",
+    "google +": "\uF613",
+    linkedin: "\uF612",
+    twitter: "\uF611",
+    approve: "\uD83D\uDC4D",
+    behance: "\uF661",
+    youtube: "\uF630",
+    blogger: "\uF622",
+    dropbox: "\uF653",
+    octocat: "\uF671",
+    android: "\uF6F3",
+    "google+": "\uF613",
+    "last fm": "\uF6B2",
+    "app net": "\uF614",
+    windows: "\uF6F2",
+    spotify: "\uF6B1",
+    flickr: "\uF640",
+    lastfm: "\uF6B2",
+    zerply: "\uF615",
+    appnet: "\uF614",
+    paypal: "\uF680",
+    tumblr: "\uF620",
+    github: "\uF670",
+    svpply: "\uF651",
+    reddit: "\uF616",
+    share: "\uF601",
+    phone: "\uD83D\uDCDE",
+    apple: "\uF8FF",
+    vimeo: "\uF631",
+    email: "\u2709",
+    steam: "\uF617",
+    quora: "\uF624",
+    "500px": "\uF642",
+    skype: "\uF6A0",
+    like: "\uD83D\uDC4D",
+    mail: "\u2709",
+    call: "\uD83D\uDCDE",
+    link: "\uD83D\uDD17",
+    rdio: "\uF6B0",
+    yelp: "\uF691",
+    etsy: "\uF682",
+    vine: "\uF633",
+    rss: "\uE310",
+  }
 
-  if (typeof ss_icons !== 'object' || typeof ss_icons !== 'object') {
-    var ss_icons = ss_set;
-    var ss_keywords = [];
+  if (typeof ss_icons !== "object" || typeof ss_icons !== "object") {
+    var ss_icons = ss_set
+    var ss_keywords = []
     for (var i in ss_set) {
-      ss_keywords.push(i);
+      ss_keywords.push(i)
     }
   } else {
     for (var i in ss_set) {
-      ss_icons[i] = ss_set[i];
-      ss_keywords.push(i);
+      ss_icons[i] = ss_set[i]
+      ss_keywords.push(i)
     }
   }
 
-  if (typeof ss_legacy !== 'function') {
+  if (typeof ss_legacy !== "function") {
     /* domready.js */
     !(function (a, b) {
-      typeof module != 'undefined'
+      typeof module != "undefined"
         ? (module.exports = b())
-        : typeof define == 'function' && typeof define.amd == 'object'
+        : typeof define == "function" && typeof define.amd == "object"
           ? define(b)
-          : (this[a] = b());
-    })('ss_ready', function (a) {
+          : (this[a] = b())
+    })("ss_ready", function (a) {
       function m(a) {
-        l = 1;
-        while ((a = b.shift())) a();
+        l = 1
+        while ((a = b.shift())) a()
       }
       var b = [],
         c,
@@ -129,17 +129,17 @@ if (
         e = document,
         f = e.documentElement,
         g = f.doScroll,
-        h = 'DOMContentLoaded',
-        i = 'addEventListener',
-        j = 'onreadystatechange',
-        k = 'readyState',
-        l = /^loade|c/.test(e[k]);
+        h = "DOMContentLoaded",
+        i = "addEventListener",
+        j = "onreadystatechange",
+        k = "readyState",
+        l = /^loade|c/.test(e[k])
       return (
         e[i] &&
           e[i](
             h,
             (c = function () {
-              (e.removeEventListener(h, c, d), m());
+              ;(e.removeEventListener(h, c, d), m())
             }),
             d
           ),
@@ -147,7 +147,7 @@ if (
           e.attachEvent(
             j,
             (c = function () {
-              /^c/.test(e[k]) && (e.detachEvent(j, c), m());
+              ;/^c/.test(e[k]) && (e.detachEvent(j, c), m())
             })
           ),
         (a = g
@@ -158,64 +158,64 @@ if (
                   : b.push(c)
                 : (function () {
                     try {
-                      f.doScroll('left');
+                      f.doScroll("left")
                     } catch (b) {
                       return setTimeout(function () {
-                        a(c);
-                      }, 50);
+                        a(c)
+                      }, 50)
                     }
-                    c();
-                  })();
+                    c()
+                  })()
             }
           : function (a) {
-              l ? a() : b.push(a);
+              l ? a() : b.push(a)
             })
-      );
-    });
+      )
+    })
 
     var ss_legacy = function (node) {
-      if ((!node) instanceof Object) return false;
+      if ((!node) instanceof Object) return false
 
       if (node.length) {
         for (var i = 0; i < node.length; i++) {
-          ss_legacy(node[i]);
+          ss_legacy(node[i])
         }
-        return;
+        return
       }
 
       if (node.value) {
-        node.value = ss_liga(node.value);
+        node.value = ss_liga(node.value)
       } else if (node.nodeValue) {
-        node.nodeValue = ss_liga(node.nodeValue);
+        node.nodeValue = ss_liga(node.nodeValue)
       } else if (node.innerHTML) {
-        node.innerHTML = ss_liga(node.innerHTML);
+        node.innerHTML = ss_liga(node.innerHTML)
       }
-    };
+    }
 
     var ss_getElementsByClassName = function (node, classname) {
       if (document.querySelectorAll) {
-        return document.querySelectorAll('.' + classname);
+        return document.querySelectorAll("." + classname)
       }
-      var a = [];
-      var re = new RegExp('(^| )' + classname + '( |$)');
-      var els = node.getElementsByTagName('*');
-      for (var i = 0, j = els.length; i < j; i++) if (re.test(els[i].className)) a.push(els[i]);
-      return a;
-    };
+      var a = []
+      var re = new RegExp("(^| )" + classname + "( |$)")
+      var els = node.getElementsByTagName("*")
+      for (var i = 0, j = els.length; i < j; i++) if (re.test(els[i].className)) a.push(els[i])
+      return a
+    }
 
     var ss_liga = function (that) {
-      var re = new RegExp(ss_keywords.join('|').replace(/[-[\]{}()*+?.,\\^$#\s]/g, '\\$&'), 'gi');
+      var re = new RegExp(ss_keywords.join("|").replace(/[-[\]{}()*+?.,\\^$#\s]/g, "\\$&"), "gi")
       return that.replace(re, function (v) {
-        return ss_icons[v.toLowerCase()];
-      });
-    };
+        return ss_icons[v.toLowerCase()]
+      })
+    }
 
     ss_ready(function () {
       if (document.getElementsByClassName) {
-        ss_legacy(document.getElementsByClassName('ss-icon'));
+        ss_legacy(document.getElementsByClassName("ss-icon"))
       } else {
-        ss_legacy(ss_getElementsByClassName(document.body, 'ss-icon'));
+        ss_legacy(ss_getElementsByClassName(document.body, "ss-icon"))
       }
-    });
+    })
   }
 }
--- js/tesselation.js
+++ js/tesselation.js
@@ -5,39 +5,39 @@
 function init(showStats) {
   // stats
   if (showStats) {
-    var stats = new Stats();
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.left = '0';
-    stats.domElement.style.top = '0';
-    document.body.appendChild(stats.domElement);
+    var stats = new Stats()
+    stats.domElement.style.position = "absolute"
+    stats.domElement.style.left = "0"
+    stats.domElement.style.top = "0"
+    document.body.appendChild(stats.domElement)
     requestAnimationFrame(function updateStats() {
-      stats.update();
-      requestAnimationFrame(updateStats);
-    });
+      stats.update()
+      requestAnimationFrame(updateStats)
+    })
   }
 
   // init
-  var svg = document.getElementById('backgroundsvg');
-  tesselation.setup(svg);
-  gradients.setup();
+  var svg = document.getElementById("backgroundsvg")
+  tesselation.setup(svg)
+  gradients.setup()
 
   var lastTransitionAt,
     transitionDelay = 5500,
-    transitionDuration = 3000;
+    transitionDuration = 3000
 
   function playNextTransition() {
-    tesselation.next(transitionDuration);
-    gradients.next(transitionDuration);
+    tesselation.next(transitionDuration)
+    gradients.next(transitionDuration)
   }
 
   function tick(time) {
     if (!lastTransitionAt || time - lastTransitionAt > transitionDelay) {
-      lastTransitionAt = time;
-      playNextTransition();
+      lastTransitionAt = time
+      playNextTransition()
     }
-    window.requestAnimationFrame(tick);
+    window.requestAnimationFrame(tick)
   }
-  window.requestAnimationFrame(tick);
+  window.requestAnimationFrame(tick)
 }
 
 //////////////////////////////
@@ -45,7 +45,7 @@ function init(showStats) {
 //////////////////////////////
 
 var calcDelaunayTriangulation = (function () {
-  var EPSILON = 1.0 / 1048576.0;
+  var EPSILON = 1.0 / 1048576.0
   function getSuperT(vertices) {
     var xMin = Number.POSITIVE_INFINITY,
       yMin = Number.POSITIVE_INFINITY,
@@ -56,23 +56,23 @@ var calcDelaunayTriangulation = (function () {
       yDiff,
       maxDiff,
       xCenter,
-      yCenter;
+      yCenter
     for (i = vertices.length; i--; ) {
-      if (vertices[i][0] < xMin) xMin = vertices[i][0];
-      if (vertices[i][0] > xMax) xMax = vertices[i][0];
-      if (vertices[i][1] < yMin) yMin = vertices[i][1];
-      if (vertices[i][1] > yMax) yMax = vertices[i][1];
+      if (vertices[i][0] < xMin) xMin = vertices[i][0]
+      if (vertices[i][0] > xMax) xMax = vertices[i][0]
+      if (vertices[i][1] < yMin) yMin = vertices[i][1]
+      if (vertices[i][1] > yMax) yMax = vertices[i][1]
     }
-    xDiff = xMax - xMin;
-    yDiff = yMax - yMin;
-    maxDiff = Math.max(xDiff, yDiff);
-    xCenter = xMin + xDiff * 0.5;
-    yCenter = yMin + yDiff * 0.5;
+    xDiff = xMax - xMin
+    yDiff = yMax - yMin
+    maxDiff = Math.max(xDiff, yDiff)
+    xCenter = xMin + xDiff * 0.5
+    yCenter = yMin + yDiff * 0.5
     return [
       [xCenter - 20 * maxDiff, yCenter - maxDiff],
       [xCenter, yCenter + 20 * maxDiff],
       [xCenter + 20 * maxDiff, yCenter - maxDiff],
-    ];
+    ]
   }
   function circumcircle(vertices, i, j, k) {
     var xI = vertices[i][0],
@@ -92,42 +92,41 @@ var calcDelaunayTriangulation = (function () {
       yMidIJ,
       yMidJK,
       xDiff,
-      yDiff;
+      yDiff
     // bail condition
     if (yDiffIJ < EPSILON && yDiffJK < EPSILON)
-      throw new Error("Can't get circumcircle since all 3 points are y-aligned");
+      throw new Error("Can't get circumcircle since all 3 points are y-aligned")
     // calc circumcircle center x/y, radius
-    m1 = -((xJ - xI) / (yJ - yI));
-    m2 = -((xK - xJ) / (yK - yJ));
-    xMidIJ = (xI + xJ) / 2.0;
-    xMidJK = (xJ + xK) / 2.0;
-    yMidIJ = (yI + yJ) / 2.0;
-    yMidJK = (yJ + yK) / 2.0;
+    m1 = -((xJ - xI) / (yJ - yI))
+    m2 = -((xK - xJ) / (yK - yJ))
+    xMidIJ = (xI + xJ) / 2.0
+    xMidJK = (xJ + xK) / 2.0
+    yMidIJ = (yI + yJ) / 2.0
+    yMidJK = (yJ + yK) / 2.0
     xCenter =
       yDiffIJ < EPSILON
         ? xMidIJ
         : yDiffJK < EPSILON
           ? xMidJK
-          : (m1 * xMidIJ - m2 * xMidJK + yMidJK - yMidIJ) / (m1 - m2);
-    yCenter =
-      yDiffIJ > yDiffJK ? m1 * (xCenter - xMidIJ) + yMidIJ : m2 * (xCenter - xMidJK) + yMidJK;
-    xDiff = xJ - xCenter;
-    yDiff = yJ - yCenter;
+          : (m1 * xMidIJ - m2 * xMidJK + yMidJK - yMidIJ) / (m1 - m2)
+    yCenter = yDiffIJ > yDiffJK ? m1 * (xCenter - xMidIJ) + yMidIJ : m2 * (xCenter - xMidJK) + yMidJK
+    xDiff = xJ - xCenter
+    yDiff = yJ - yCenter
     // return
-    return { i: i, j: j, k: k, x: xCenter, y: yCenter, r: xDiff * xDiff + yDiff * yDiff };
+    return { i: i, j: j, k: k, x: xCenter, y: yCenter, r: xDiff * xDiff + yDiff * yDiff }
   }
   function dedupeEdges(edges) {
-    var i, j, a, b, m, n;
+    var i, j, a, b, m, n
     for (j = edges.length; j; ) {
-      b = edges[--j];
-      a = edges[--j];
+      b = edges[--j]
+      a = edges[--j]
       for (i = j; i; ) {
-        n = edges[--i];
-        m = edges[--i];
+        n = edges[--i]
+        m = edges[--i]
         if ((a === m && b === n) || (a === n && b === m)) {
-          edges.splice(j, 2);
-          edges.splice(i, 2);
-          break;
+          edges.splice(j, 2)
+          edges.splice(i, 2)
+          break
         }
       }
     }
@@ -145,153 +144,145 @@ var calcDelaunayTriangulation = (function () {
       dy,
       a,
       b,
-      c;
+      c
     // bail if too few / too many verts
-    if (n < 3 || n > 2000) return [];
+    if (n < 3 || n > 2000) return []
     // copy verts and sort indices by x-position
-    vertices = vertices.slice(0);
-    indices = new Array(n);
-    for (i = n; i--; ) indices[i] = i;
+    vertices = vertices.slice(0)
+    indices = new Array(n)
+    for (i = n; i--; ) indices[i] = i
     indices.sort(function (i, j) {
-      return vertices[j][0] - vertices[i][0];
-    });
+      return vertices[j][0] - vertices[i][0]
+    })
     // supertriangle
-    st = getSuperT(vertices);
-    vertices.push(st[0], st[1], st[2]);
+    st = getSuperT(vertices)
+    vertices.push(st[0], st[1], st[2])
     // init candidates/locked tris list
-    candidates = [circumcircle(vertices, n + 0, n + 1, n + 2)];
-    locked = [];
-    edges = [];
+    candidates = [circumcircle(vertices, n + 0, n + 1, n + 2)]
+    locked = []
+    edges = []
     // scan left to right
     for (i = indices.length; i--; edges.length = 0) {
-      c = indices[i];
+      c = indices[i]
       // check candidates tris against point
       for (j = candidates.length; j--; ) {
         // lock tri if point to right of circumcirc
-        dx = vertices[c][0] - candidates[j].x;
+        dx = vertices[c][0] - candidates[j].x
         if (dx > 0.0 && dx * dx > candidates[j].r) {
-          locked.push(candidates[j]);
-          candidates.splice(j, 1);
-          continue;
+          locked.push(candidates[j])
+          candidates.splice(j, 1)
+          continue
         }
         // point outside circumcirc = leave candidates
-        dy = vertices[c][1] - candidates[j].y;
-        if (dx * dx + dy * dy - candidates[j].r > EPSILON) continue;
+        dy = vertices[c][1] - candidates[j].y
+        if (dx * dx + dy * dy - candidates[j].r > EPSILON) continue
         // point inside circumcirc = break apart, save edges
-        edges.push(
-          candidates[j].i,
-          candidates[j].j,
-          candidates[j].j,
-          candidates[j].k,
-          candidates[j].k,
-          candidates[j].i
-        );
-        candidates.splice(j, 1);
+        edges.push(candidates[j].i, candidates[j].j, candidates[j].j, candidates[j].k, candidates[j].k, candidates[j].i)
+        candidates.splice(j, 1)
       }
       // new candidates from broken edges
-      dedupeEdges(edges);
+      dedupeEdges(edges)
       for (j = edges.length; j; ) {
-        b = edges[--j];
-        a = edges[--j];
-        candidates.push(circumcircle(vertices, a, b, c));
+        b = edges[--j]
+        a = edges[--j]
+        candidates.push(circumcircle(vertices, a, b, c))
       }
     }
     // close candidates tris, remove tris touching supertri verts
-    for (i = candidates.length; i--; ) locked.push(candidates[i]);
-    candidates.length = 0;
+    for (i = candidates.length; i--; ) locked.push(candidates[i])
+    candidates.length = 0
     for (i = locked.length; i--; )
-      if (locked[i].i < n && locked[i].j < n && locked[i].k < n)
-        candidates.push(locked[i].i, locked[i].j, locked[i].k);
+      if (locked[i].i < n && locked[i].j < n && locked[i].k < n) candidates.push(locked[i].i, locked[i].j, locked[i].k)
     // done
-    return candidates;
-  };
-})();
+    return candidates
+  }
+})()
 
 var tesselation = (function () {
-  var svg, svgW, svgH, prevGroup;
+  var svg, svgW, svgH, prevGroup
 
   function createRandomTesselation() {
-    var wW = window.innerWidth;
-    var wH = window.innerHeight;
+    var wW = window.innerWidth
+    var wH = window.innerHeight
 
     var gridSpacing = 250,
-      scatterAmount = 0.75;
-    var gridSize, i, x, y;
+      scatterAmount = 0.75
+    var gridSize, i, x, y
 
     if (wW / wH > svgW / svgH) {
       // window wider than svg = use width for gridSize
-      gridSize = (gridSpacing * svgW) / wW;
+      gridSize = (gridSpacing * svgW) / wW
     } else {
       // window taller than svg = use height for gridSize
-      gridSize = (gridSpacing * svgH) / wH;
+      gridSize = (gridSpacing * svgH) / wH
     }
 
-    var vertices = [];
+    var vertices = []
     var xOffset = (svgW % gridSize) / 2,
-      yOffset = (svgH % gridSize) / 2;
+      yOffset = (svgH % gridSize) / 2
     for (x = Math.floor(svgW / gridSize) + 1; x >= -1; x--) {
       for (y = Math.floor(svgH / gridSize) + 1; y >= -1; y--) {
         vertices.push([
           xOffset + gridSize * (x + scatterAmount * (Math.random() - 0.5)),
           yOffset + gridSize * (y + scatterAmount * (Math.random() - 0.5)),
-        ]);
+        ])
       }
     }
 
-    var triangles = calcDelaunayTriangulation(vertices);
+    var triangles = calcDelaunayTriangulation(vertices)
 
-    var group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-    var polygon;
+    var group = document.createElementNS("http://www.w3.org/2000/svg", "g")
+    var polygon
     for (i = triangles.length; i; ) {
-      polygon = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+      polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon")
       polygon.setAttribute(
-        'points',
+        "points",
         vertices[triangles[--i]][0] +
-          ',' +
+          "," +
           vertices[triangles[i]][1] +
-          ' ' +
+          " " +
           vertices[triangles[--i]][0] +
-          ',' +
+          "," +
           vertices[triangles[i]][1] +
-          ' ' +
+          " " +
           vertices[triangles[--i]][0] +
-          ',' +
+          "," +
           vertices[triangles[i]][1]
-      );
-      group.appendChild(polygon);
+      )
+      group.appendChild(polygon)
     }
 
-    return group;
+    return group
   }
 
   return {
     setup: function (svgElement) {
-      svg = svgElement;
-      var vb = svg.getAttribute('viewBox').split(/\D/g);
-      svgW = vb[2];
-      svgH = vb[3];
+      svg = svgElement
+      var vb = svg.getAttribute("viewBox").split(/\D/g)
+      svgW = vb[2]
+      svgH = vb[3]
     },
     next: function (t) {
-      var toRemove, i, n;
-      t /= 1000;
+      var toRemove, i, n
+      t /= 1000
 
       if (prevGroup && prevGroup.children && prevGroup.children.length) {
-        toRemove = prevGroup;
-        n = toRemove.children.length;
+        toRemove = prevGroup
+        n = toRemove.children.length
         for (i = n; i--; ) {
-          TweenMax.to(toRemove.children[i], t * 0.4, { opacity: 0, delay: t * ((0.3 * i) / n) });
+          TweenMax.to(toRemove.children[i], t * 0.4, { opacity: 0, delay: t * ((0.3 * i) / n) })
         }
         TweenMax.delayedCall(
           t * (0.7 + 0.05),
           function (group) {
-            svg.removeChild(group);
+            svg.removeChild(group)
           },
           [toRemove],
           this
-        );
+        )
       }
-      var g = createRandomTesselation();
-      n = g.children.length;
+      var g = createRandomTesselation()
+      n = g.children.length
       for (i = n; i--; ) {
         TweenMax.fromTo(
           g.children[i],
@@ -302,86 +293,86 @@ var tesselation = (function () {
             delay: t * ((0.3 * i) / n + 0.3),
             ease: Back.easeOut,
           }
-        );
+        )
       }
-      svg.appendChild(g);
-      prevGroup = g;
+      svg.appendChild(g)
+      prevGroup = g
     },
-  };
-})();
+  }
+})()
 
 //////////////////////////////
 // Gradients
 //////////////////////////////
 
 var gradients = (function () {
-  var grad1, grad2, showingGrad1;
+  var grad1, grad2, showingGrad1
 
   // using colors from IBM Design Colors this time
   var colors = [
     // 14 colors - use 3-5 span
-    '#3c6df0', // ultramarine50
-    '#12a3b4', // aqua40
-    '#00a78f', // teal40
-    '#00aa5e', // green40
-    '#81b532', // lime30
-    '#e3bc13', // yellow20
-    '#ffb000', // gold20
-    '#fe8500', // orange30
-    '#fe6100', // peach40
-    '#e62325', // red50
-    '#dc267f', // magenta50
-    '#c22dd5', // purple50
-    '#9753e1', // violet50
-    '#5a3ec8', // indigo60
-  ];
+    "#3c6df0", // ultramarine50
+    "#12a3b4", // aqua40
+    "#00a78f", // teal40
+    "#00aa5e", // green40
+    "#81b532", // lime30
+    "#e3bc13", // yellow20
+    "#ffb000", // gold20
+    "#fe8500", // orange30
+    "#fe6100", // peach40
+    "#e62325", // red50
+    "#dc267f", // magenta50
+    "#c22dd5", // purple50
+    "#9753e1", // violet50
+    "#5a3ec8", // indigo60
+  ]
 
   function assignRandomColors(gradObj) {
-    var rA = Math.floor(colors.length * Math.random());
-    var rB = Math.floor(Math.random() * 3) + 3; // [3 - 5]
-    rB = (rA + rB * (Math.random() < 0.5 ? -1 : 1) + colors.length) % colors.length;
-    gradObj.stopA.setAttribute('stop-color', colors[rA]);
-    gradObj.stopB.setAttribute('stop-color', colors[rB]);
+    var rA = Math.floor(colors.length * Math.random())
+    var rB = Math.floor(Math.random() * 3) + 3 // [3 - 5]
+    rB = (rA + rB * (Math.random() < 0.5 ? -1 : 1) + colors.length) % colors.length
+    gradObj.stopA.setAttribute("stop-color", colors[rA])
+    gradObj.stopB.setAttribute("stop-color", colors[rB])
   }
 
   return {
     setup: function () {
-      showingGrad1 = false;
+      showingGrad1 = false
       grad1 = {
-        stopA: document.getElementById('stop1a'),
-        stopB: document.getElementById('stop1b'),
-        rect: document.getElementById('rect1'),
-      };
+        stopA: document.getElementById("stop1a"),
+        stopB: document.getElementById("stop1b"),
+        rect: document.getElementById("rect1"),
+      }
       grad2 = {
-        stopA: document.getElementById('stop2a'),
-        stopB: document.getElementById('stop2b'),
-        rect: document.getElementById('rect2'),
-      };
-      grad1.rect.style.opacity = 0;
-      grad2.rect.style.opacity = 0;
+        stopA: document.getElementById("stop2a"),
+        stopB: document.getElementById("stop2b"),
+        rect: document.getElementById("rect2"),
+      }
+      grad1.rect.style.opacity = 0
+      grad2.rect.style.opacity = 0
     },
     next: function (t) {
-      t /= 1000;
+      t /= 1000
 
-      var show, hide;
+      var show, hide
       if (showingGrad1) {
-        hide = grad1;
-        show = grad2;
+        hide = grad1
+        show = grad2
       } else {
-        hide = grad2;
-        show = grad1;
+        hide = grad2
+        show = grad1
       }
-      showingGrad1 = !showingGrad1;
+      showingGrad1 = !showingGrad1
 
-      TweenMax.to(hide.rect, 0.55 * t, { opacity: 0, delay: 0.2 * t, ease: Sine.easeOut });
-      assignRandomColors(show);
-      TweenMax.to(show.rect, 0.65 * t, { opacity: 1, ease: Sine.easeIn });
+      TweenMax.to(hide.rect, 0.55 * t, { opacity: 0, delay: 0.2 * t, ease: Sine.easeOut })
+      assignRandomColors(show)
+      TweenMax.to(show.rect, 0.65 * t, { opacity: 1, ease: Sine.easeIn })
     },
-  };
-})();
+  }
+})()
 
 //////////////////////////////
 // Start
 //////////////////////////////
 
-init();
+init()
